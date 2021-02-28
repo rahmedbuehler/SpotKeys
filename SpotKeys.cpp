01377dc45a629f07e58d4d4f188249baead986f6
rab <r.ahmedbuehler@gmail.com>
2021-02-27 16:05:39 -0800

    Register arrows as hotkeys

--- SpotKeys.cpp
+++ SpotKeys.cpp
@@ -1,20 +1,41 @@
 // SpotKeys.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <Windows.h>
+
+int register_arrow(int id, UINT vk)
+{
+    if (RegisterHotKey(NULL, id, MOD_NOREPEAT, vk))
+    {
+        std::cout << "Hotkey "<< vk <<" registered.\n";
+        return 0;
+    }
+    else
+    {
+        std::cout << "Error code " << GetLastError();
+        return 1;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"
+    int id = 0;
+    int arrows[4]{};
+    arrows[0] = VK_LEFT;
+    arrows[1] = VK_RIGHT;
+    arrows[2] = VK_DOWN;
+    arrows[3] = VK_UP;
+    for (int i{ 0 }; i < 4; ++i)
+    {
+        register_arrow(i, arrows[i]);
+    }
+    
+    MSG msg = { 0 };
+    while (GetMessage(&msg, NULL, 0, 0) != 0)
+    {
+        std::cout << "\nButton " << msg.wParam <<" Pressed";
+        //if (msg.message == WM_HOTKEY)
+            //std::cout << " -> WM_HOTKEY received";
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
